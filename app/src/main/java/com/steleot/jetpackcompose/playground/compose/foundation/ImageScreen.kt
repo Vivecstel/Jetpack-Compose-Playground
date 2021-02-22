aed006b833754f3c7c7d15523cf17fbed86f86e8
Stelios Elaiotrivaris <steleot@hotmail.com>
2021-02-22 18:20:53 +0200

    minor fixes

--- app/src/main/java/com/steleot/jetpackcompose/playground/compose/foundation/ImageScreen.kt
+++ app/src/main/java/com/steleot/jetpackcompose/playground/compose/foundation/ImageScreen.kt
@@ -1,7 +1,26 @@
 package com.steleot.jetpackcompose.playground.compose.foundation
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.imageFromResource
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.steleot.jetpackcompose.playground.FoundationNavRoutes
+import com.steleot.jetpackcompose.playground.R
 import com.steleot.jetpackcompose.playground.compose.reusable.DefaultScaffold
 import java.util.*
 
@@ -10,6 +29,56 @@ fun ImageScreen() {
     DefaultScaffold(
         title = FoundationNavRoutes.Image.capitalize(Locale.getDefault())
     ) {
-
+        Column(
+            modifier = Modifier.fillMaxSize().padding(32.dp),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            DefaultImage()
+            ColorPainterImage()
+            BitmapImage()
+            VectorImage()
+        }
     }
+}
+
+@Preview
+@Composable
+private fun DefaultImage() {
+    Image(
+        painter = painterResource(R.drawable.ic_baseline_alarm_24dp),
+        contentDescription = "Default",
+        colorFilter = ColorFilter.tint(Color.Blue),
+    )
+}
+
+@Preview
+@Composable
+private fun ColorPainterImage() {
+    Image(
+        painter = ColorPainter(Color.Magenta),
+        contentDescription = "Color",
+        modifier = Modifier.preferredSize(64.dp)
+    )
+}
+
+@Preview
+@Composable
+private fun BitmapImage() {
+    val context = LocalContext.current
+    Image(
+        bitmap = imageFromResource(context.resources, R.drawable.ic_baseline_alarm_24dp),
+        contentDescription = "Bitmap",
+        colorFilter = ColorFilter.tint(Color.Blue),
+        modifier = Modifier.preferredSize(64.dp),
+    )
+}
+
+@Preview
+@Composable
+private fun VectorImage() {
+    Image(
+        imageVector = Icons.Filled.Build,
+        contentDescription = "Vector"
+    )
 }
--- app/src/main/java/com/steleot/jetpackcompose/playground/compose/material/ExtendedFloatingActionButtonScreen.kt
+++ app/src/main/java/com/steleot/jetpackcompose/playground/compose/material/ExtendedFloatingActionButtonScreen.kt
@@ -1,6 +1,23 @@
 package com.steleot.jetpackcompose.playground.compose.material
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.ExtendedFloatingActionButton
+import androidx.compose.material.FloatingActionButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.steleot.jetpackcompose.playground.MaterialNavRoutes
 import com.steleot.jetpackcompose.playground.compose.reusable.DefaultScaffold
 import java.util.*
@@ -10,6 +27,80 @@ fun ExtendedFloatingActionButtonScreen() {
     DefaultScaffold(
         title = MaterialNavRoutes.ExtendedFloatingActionButton.capitalize(Locale.getDefault())
     ) {
-
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            IconExtendedFloatingActionButton()
+            ColoredExtendedFloatingActionButton()
+            ElevatedExtendedFloatingActionButton()
+            ShapeExtendedFloatingActionButton()
+        }
     }
+}
+
+@Preview
+@Composable
+private fun IconExtendedFloatingActionButton() {
+    ExtendedFloatingActionButton(
+        text = { Text(text = "Star") },
+        onClick = {},
+        icon = {
+            Icon(
+                imageVector = Icons.Outlined.Star,
+                contentDescription = "Icon Extended Floating Action Button"
+            )
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun ColoredExtendedFloatingActionButton() {
+    ExtendedFloatingActionButton(
+        text = { Text(text = "Star") },
+        onClick = {},
+        backgroundColor = Color.Red,
+        contentColor = Color.White,
+        icon = {
+            Icon(
+                imageVector = Icons.Filled.Star,
+                contentDescription = "Colored Extended Floating Action Button"
+            )
+        },
+    )
+}
+
+@Preview
+@Composable
+private fun ElevatedExtendedFloatingActionButton() {
+    ExtendedFloatingActionButton(
+        text = { Text(text = "Star") },
+        onClick = {},
+        shape = CutCornerShape(15.dp),
+        elevation = FloatingActionButtonDefaults.elevation(20.dp),
+        icon = {
+            Icon(
+                imageVector = Icons.Filled.Star,
+                contentDescription = "Elevated Extended Floating Action Button"
+            )
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun ShapeExtendedFloatingActionButton() {
+    ExtendedFloatingActionButton(
+        text = { Text(text = "Star") },
+        onClick = {},
+        shape = RectangleShape,
+        icon = {
+            Icon(
+                imageVector = Icons.Filled.Star,
+                contentDescription = "my description"
+            )
+        }
+    )
 }
