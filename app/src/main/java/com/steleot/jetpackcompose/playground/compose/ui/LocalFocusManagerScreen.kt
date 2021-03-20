1801828853a5c67d7475f1160c67853b1b3d95bd
Stelios Elaiotrivaris <steleot@hotmail.com>
2021-03-20 18:48:43 +0200

    minor fixes

--- app/src/main/java/com/steleot/jetpackcompose/playground/compose/ui/LocalFocusManagerScreen.kt
+++ app/src/main/java/com/steleot/jetpackcompose/playground/compose/ui/LocalFocusManagerScreen.kt
@@ -1,7 +1,65 @@
 package com.steleot.jetpackcompose.playground.compose.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import com.steleot.jetpackcompose.playground.UiNavRoutes
+import com.steleot.jetpackcompose.playground.compose.reusable.DefaultScaffold
+import java.util.Locale
+
+private const val Url = "ui/LocalFocusManagerScreen.kt"
 
 @Composable
 fun LocalFocusManagerScreen() {
+    DefaultScaffold(
+        title = UiNavRoutes.LocalFocusManager.capitalize(Locale.getDefault()),
+        link = Url,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LocalFocusManagerExample()
+        }
+    }
+}
+
+@Composable
+private fun LocalFocusManagerExample() {
+    val focusManager = LocalFocusManager.current
+    val (value1, setValue1) = remember { mutableStateOf("value1") }
+    val (value2, setValue2) = remember { mutableStateOf("value2") }
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = value1,
+            onValueChange = setValue1
+        )
+        TextField(
+            value = value2,
+            onValueChange = setValue2
+        )
+        Button(onClick = {
+            focusManager.clearFocus()
+        }) {
+            Text(text = "Tap to clear focus")
+        }
+    }
 }
--- app/src/main/java/com/steleot/jetpackcompose/playground/compose/ui/LocalFontLoaderScreen.kt
+++ app/src/main/java/com/steleot/jetpackcompose/playground/compose/ui/LocalFontLoaderScreen.kt
@@ -1,7 +1,50 @@
 package com.steleot.jetpackcompose.playground.compose.ui
 
+import android.graphics.Typeface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFontLoader
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import com.steleot.jetpackcompose.playground.R
+import com.steleot.jetpackcompose.playground.UiNavRoutes
+import com.steleot.jetpackcompose.playground.compose.reusable.DefaultScaffold
+import java.util.Locale
+
+private const val Url = "ui/LocalFontLoaderScreen.kt"
 
 @Composable
 fun LocalFontLoaderScreen() {
+    DefaultScaffold(
+        title = UiNavRoutes.LocalFontLoader.capitalize(Locale.getDefault()),
+        link = Url,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LocalFontLoaderExample()
+        }
+    }
+}
+
+@Composable
+private fun LocalFontLoaderExample() {
+    val fontLoader = LocalFontLoader.current
+    val typeface = fontLoader.load(Font(R.font.ballet_font)) as Typeface
+    Text(
+        text = "Local Font Loader with ballet font",
+        fontFamily = FontFamily(typeface)
+    )
 }
