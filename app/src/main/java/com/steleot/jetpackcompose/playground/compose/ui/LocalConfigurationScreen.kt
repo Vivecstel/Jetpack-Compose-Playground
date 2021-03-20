8e866b8a90795bbaaba690877c02b1d16fe15d48
Stelios Elaiotrivaris <steleot@hotmail.com>
2021-03-20 16:47:35 +0200

    minor fixes

--- app/src/main/java/com/steleot/jetpackcompose/playground/compose/ui/LocalConfigurationScreen.kt
+++ app/src/main/java/com/steleot/jetpackcompose/playground/compose/ui/LocalConfigurationScreen.kt
@@ -1,7 +1,53 @@
 package com.steleot.jetpackcompose.playground.compose.ui
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import com.steleot.jetpackcompose.playground.UiNavRoutes
+import com.steleot.jetpackcompose.playground.compose.reusable.DefaultScaffold
+import java.util.Locale
+
+private const val Url = "ui/LocalConfigurationScreen.kt"
 
 @Composable
 fun LocalConfigurationScreen() {
+    DefaultScaffold(
+        title = UiNavRoutes.LocalConfiguration.capitalize(Locale.getDefault()),
+        link = Url,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LocalConfigurationExample()
+        }
+    }
+}
+
+@Suppress("DEPRECATION")
+@Composable
+private fun LocalConfigurationExample() {
+    val configuration = LocalConfiguration.current
+    Text(text = """
+        Local configuration values:
+        densityDpi: ${configuration.densityDpi}
+        fontScale: ${configuration.fontScale}
+        locale: ${configuration.locale}
+        orientation: ${when (configuration.orientation){
+            Configuration.ORIENTATION_PORTRAIT -> "Portrait"
+            else -> "Landscape"
+        } }
+        etc.
+    """.trimIndent())
 }
--- app/src/main/java/com/steleot/jetpackcompose/playground/compose/ui/LocalContextScreen.kt
+++ app/src/main/java/com/steleot/jetpackcompose/playground/compose/ui/LocalContextScreen.kt
@@ -1,7 +1,45 @@
 package com.steleot.jetpackcompose.playground.compose.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.steleot.jetpackcompose.playground.UiNavRoutes
+import com.steleot.jetpackcompose.playground.compose.reusable.DefaultScaffold
+import java.util.Locale
+
+private const val Url = "ui/LocalContextScreen.kt"
 
 @Composable
 fun LocalContextScreen() {
+    DefaultScaffold(
+        title = UiNavRoutes.LocalContext.capitalize(Locale.getDefault()),
+        link = Url,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LocalContextExample()
+        }
+    }
+}
+
+@Composable
+private fun LocalContextExample() {
+    val context = LocalContext.current
+    Text(
+        text = "Local Context example cache dir:\n${context.cacheDir}",
+        textAlign = TextAlign.Center
+    )
 }
