c5df2a8ec77ad30c3f1913879804257fcdd31e8a
Stelios Elaiotrivaris <steleot@hotmail.com>
2021-03-20 17:13:24 +0200

    minor fixes

--- app/src/main/java/com/steleot/jetpackcompose/playground/compose/ui/LocalLayoutDirectionScreen.kt
+++ app/src/main/java/com/steleot/jetpackcompose/playground/compose/ui/LocalLayoutDirectionScreen.kt
@@ -1,7 +1,41 @@
 package com.steleot.jetpackcompose.playground.compose.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.dp
+import com.steleot.jetpackcompose.playground.UiNavRoutes
+import com.steleot.jetpackcompose.playground.compose.reusable.DefaultScaffold
+import java.util.Locale
+
+private const val Url = "ui/LocalLayoutDirectionScreen.kt"
 
 @Composable
 fun LocalLayoutDirectionScreen() {
+    DefaultScaffold(
+        title = UiNavRoutes.LocalLayoutDirection.capitalize(Locale.getDefault()),
+        link = Url,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LocalLayoutDirectionScreenExample()
+        }
+    }
+}
+
+@Composable
+private fun LocalLayoutDirectionScreenExample() {
+    val layoutDirection = LocalLayoutDirection.current
+    Text(text = "Local layout Direction is: $layoutDirection")
 }
--- app/src/main/java/com/steleot/jetpackcompose/playground/compose/ui/LocalWindowInfoScreen.kt
+++ app/src/main/java/com/steleot/jetpackcompose/playground/compose/ui/LocalWindowInfoScreen.kt
@@ -1,7 +1,41 @@
 package com.steleot.jetpackcompose.playground.compose.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.unit.dp
+import com.steleot.jetpackcompose.playground.UiNavRoutes
+import com.steleot.jetpackcompose.playground.compose.reusable.DefaultScaffold
+import java.util.Locale
+
+private const val Url = "ui/LocalWindowInfoScreen.kt"
 
 @Composable
 fun LocalWindowInfoScreen() {
+    DefaultScaffold(
+        title = UiNavRoutes.LocalWindowInfo.capitalize(Locale.getDefault()),
+        link = Url,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LocalWindowInfoExample()
+        }
+    }
+}
+
+@Composable
+private fun LocalWindowInfoExample() {
+    val windowInfo = LocalWindowInfo.current
+    Text(text = "Local Window Info:\nisWindowFocused: ${windowInfo.isWindowFocused}")
 }
