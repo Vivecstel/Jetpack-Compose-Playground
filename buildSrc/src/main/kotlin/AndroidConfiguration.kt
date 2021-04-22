c6ce681fba7904b8839861156cd98896d6fd05f9
Stelios Elaiotrivaris <steleot@hotmail.com>
2021-04-22 20:58:12 +0300

    version update

--- buildSrc/src/main/kotlin/AndroidConfiguration.kt
+++ buildSrc/src/main/kotlin/AndroidConfiguration.kt
@@ -5,6 +5,6 @@ object AndroidConfiguration {
     const val targetSdk = compileSdk
 
     const val appId = "com.steleot.jetpackcompose.playground"
-    const val versionCode = 8
-    const val versionName = "1.5.0"
+    const val versionCode = 9
+    const val versionName = "1.6.0"
 }
