9408c473a93406ebdf89089ccc4b678fe28ed9d4
Stelios Elaiotrivaris <steleot@hotmail.com>
2021-08-14 17:15:28 +0300

    version update

--- buildSrc/src/main/kotlin/AndroidConfiguration.kt
+++ buildSrc/src/main/kotlin/AndroidConfiguration.kt
@@ -8,8 +8,8 @@ object AndroidConfiguration {
     const val targetSdk = compileSdk
 
     const val appId = "com.steleot.jetpackcompose.playground"
-    const val versionCode = 34
-    const val versionName = "2.8.0"
+    const val versionCode = 35
+    const val versionName = "2.9.0"
 
     private const val branch = "develop"
     const val baseUrl = "\"https://github.com/Vivecstel/Jetpack-Compose-Playground/blob/$branch/" +
