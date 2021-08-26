515e4184aea012163825508386dbca37ecf1dc17
Stelios Elaiotrivaris <steleot@hotmail.com>
2021-08-26 19:42:17 +0300

    Added play publisher gradle to easily upload bundles to play store.

--- buildSrc/src/main/kotlin/AndroidConfiguration.kt
+++ buildSrc/src/main/kotlin/AndroidConfiguration.kt
@@ -14,4 +14,10 @@ object AndroidConfiguration {
     private const val branch = "develop"
     const val baseUrl = "\"https://github.com/Vivecstel/Jetpack-Compose-Playground/blob/$branch/" +
             "app/src/main/java/com/steleot/jetpackcompose/playground/compose/\""
+
+    const val releaseName = "$versionCode ($versionName)"
+    const val serviceAccountCredentials = "jetpack-compose-playground-play-publishing.json"
+    const val artifactFile = "build/outputs/bundle/release/app-release.aab"
+    const val track = "production"
+    const val updatePriority = 2
 }
