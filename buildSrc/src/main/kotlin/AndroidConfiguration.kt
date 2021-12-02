6c3c2ce311f362b2dd11d99035cae6498c5359ce
Stelios Elaiotrivaris <steleot@hotmail.com>
2021-12-02 10:33:38 +0200

    update version

--- buildSrc/src/main/kotlin/AndroidConfiguration.kt
+++ buildSrc/src/main/kotlin/AndroidConfiguration.kt
@@ -7,8 +7,8 @@ object AndroidConfiguration {
     const val targetSdk = compileSdk
 
     const val appId = "com.steleot.jetpackcompose.playground"
-    const val versionCode = 49
-    const val versionName = "4.1.0"
+    const val versionCode = 50
+    const val versionName = "4.2.0"
 
     private const val branch = "develop"
     const val baseUrl = "\"https://github.com/Vivecstel/Jetpack-Compose-Playground/blob/$branch/" +
@@ -18,5 +18,5 @@ object AndroidConfiguration {
     const val serviceAccountCredentials = "jetpack-compose-playground-play-publishing.json"
     const val artifactFile = "build/outputs/bundle/release/app-release.aab"
     const val track = "production"
-    const val updatePriority = 4
+    const val updatePriority = 2
 }
