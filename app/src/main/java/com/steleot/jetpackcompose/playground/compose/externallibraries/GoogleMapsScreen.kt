d95512618421de3e5864551f3a9f1373aa183730
Stelios Elaiotrivaris <steleot@hotmail.com>
2022-02-10 11:30:51 +0200

    Updated libraries. Added shortcuts. Updated google maps example.

--- app/src/main/java/com/steleot/jetpackcompose/playground/compose/externallibraries/GoogleMapsScreen.kt
+++ app/src/main/java/com/steleot/jetpackcompose/playground/compose/externallibraries/GoogleMapsScreen.kt
@@ -11,10 +11,9 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MapProperties
-import com.google.maps.android.compose.MapType
-import com.google.maps.android.compose.MapUiSettings
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.*
 import com.steleot.jetpackcompose.playground.compose.reusable.DefaultScaffold
 import com.steleot.jetpackcompose.playground.navigation.ExternalLibrariesNavRoutes
 
@@ -33,18 +32,25 @@ fun GoogleMapsScreen() {
 @Composable
 private fun GoogleMapsExample() {
     var isMapLoaded by remember { mutableStateOf(false) }
+    val athens = LatLng(37.983810, 23.727539)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(athens, 8f)
+    }
     Box(Modifier.fillMaxSize()) {
         GoogleMap(
             modifier = Modifier.matchParentSize(),
+            cameraPositionState = cameraPositionState,
             onMapLoaded = {
                 isMapLoaded = true
             },
             properties = MapProperties(
                 mapType = MapType.NORMAL,
-                maxZoomPreference = 10f,
             ),
             uiSettings = MapUiSettings(compassEnabled = false),
-        )
+        ) {
+            Marker(position = athens, title = "Athens")
+            Marker(position = LatLng(40.629269, 22.947412), title = "Thessaloniki")
+        }
         if (!isMapLoaded) {
             AnimatedVisibility(
                 modifier = Modifier
